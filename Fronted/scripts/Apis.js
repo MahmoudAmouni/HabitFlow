7e1a15bbf65df4258d251e54ce4dbd8afd763f00
Mahmoud Amouni <mhmdamoun0@gmail.com>
2025-11-15 17:25:01 +0200

    Frontend -> connected the signup with the dataBase

---
+++ Fronted/scripts/Apis.js
@@ -0,0 +1,46 @@
+export async function createUser(
+  username,
+  email,
+  password,
+  height,
+  weight,
+  gender
+) {
+  try {
+    const res = await fetch("http://localhost/HabitFlow/Backend/users/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: username,
+        email,
+        password,
+        height,
+        weight,
+        gender,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getUserByEmail(email, password) {
+  try {
+    const res = await fetch("", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        email,
+        password,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
